e422b51354491064f45250d0f5452b25d9903822
cyc-987 <2516803593@qq.com>
2025-09-19 15:23:13 +0800

    add: 新增统计信息

--- push.py
+++ push.py
@@ -12,13 +12,27 @@ class DingBot:
             return
         
         # Prepare the message
+        total_num = 0
+        total_available = 0
+        total_used = 0
+        total_error = 0
         message = f"> 抓取时间 - {time}\n"
         for site in json_content:
             message += f"##### **{site['site_name']}**\n"
             message += f"**{site['site_available']}**可用/"
+            total_available += site['site_available']
             message += f"**{site['site_used']}**已用/"
+            total_used += site['site_used']
             message += f"**{site['site_error']}**错误/"
+            total_error += site['site_error']
             message += f"**{site['site_total']}**总端口\n\n"
+            total_num += site['site_total']
+        message += f"##### **总计**\n"
+        message += f"**{total_available}**可用/"
+        message += f"**{total_used}**已用/"
+        message += f"**{total_error}**错误/"
+        message += f"**{total_num}**总端口\n\n"
+        message += f"使用率: **{(total_used / (total_num-total_error) * 100) if (total_num-total_error) > 0 else "NaN":.2f}%**\n\n"
         message += "> 有未收录站点请发送邮箱至 <群主邮箱>\n"
         # Send the message
         try:
